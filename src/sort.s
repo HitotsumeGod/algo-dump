1869fa627774b326ede146bf3f488e1cfdce4235
peternaylor <peter@oldtiogafarm.com>
2025-05-16 12:13:47 -0400

    working on sorted checker

--- src/sort.s
+++ src/sort.s
@@ -2,9 +2,24 @@ section		.data
 	vals	dw 5, 4, 3, 2, 3
 section		.text
 global 		_start
+check_if_sorted:
+	mov	edi, vals-2
+sortit:
+	add	edi, 2
+	mov	ax, [edi]
+	mov	bx, [edi+2]
+	cmp	ax, 0x0
+	je 	done
+	cmp	ax, bx
+	jbe	sortit
+	mov	r8b, 0x99
+	ret
+done:
+	xor	r8b, r8b
+	ret
 _start:
 	mov	edi, vals
-	mov	cl, 5
+	mov	cl, 5	
 loop:
 	mov 	ax, [edi]
 	cmp	ax, [edi+2]	
@@ -16,6 +31,9 @@ skip:
 	add	edi, 2
 	dec	cl
 	jnz	loop
+	call 	check_if_sorted
+	cmp	r8b, 0x99
+	je	_start
 exit:
 	mov	al, 60
 	mov	dil, 0
